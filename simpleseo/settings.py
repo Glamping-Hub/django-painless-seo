083d82fafcbcb9820e8b1111444883f3de9c1069
Héctor Giner <hectorginer@gmail.com>
2014-07-21 11:34:52 +0200

    Settings for non-multilanguage sites

--- simpleseo/settings.py
+++ simpleseo/settings.py
@@ -10,6 +10,13 @@ if FALLBACK_TITLE is None:
 if FALLBACK_DESCRIPTION is None:
     raise ImproperlyConfigured('SEO_DEFAULT_DESCRIPTION is not defined in settings.')
 
-SEO_LANGUAGES = getattr(settings, 'LANGUAGES', None)
+I18N = getattr(settings, 'USE_I18N')
+if I18N:
+    SEO_LANGUAGES = getattr(settings, 'LANGUAGES', None)
+    if not SEO_LANGUAGES:
+        raise ImproperlyConfigured('If USE_I18N is set to True, you need to define LANGUAGES in settings.')
+else:
+    DEFAULT_LANG_CODE = getattr(settings, 'LANGUAGE_CODE')
+    SEO_LANGUAGES = ((DEFAULT_LANG_CODE[:2], DEFAULT_LANG_CODE), )
 
 SEO_MODELS = getattr(settings, 'SEO_MODELS', [])
