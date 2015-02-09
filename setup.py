e53c1e4292e6bfa6ea5ba5dcaef4fc251c6f7346
Alejandro Blanco <alejandro.b.e@gmail.com>
2015-02-09 12:39:25 +0100

    Fix included files in the package

--- setup.py
+++ setup.py
@@ -1,14 +1,38 @@
 # Copyright (C) 2014 Glamping Hub (https://glampinghub.com)
 # License: BSD 3-Clause
 
-from setuptools import setup
+import fnmatch
+import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+def recursive_include(directory, patterns):
+    result = []
+    for root, dirs, files in os.walk(directory):
+        child_root = root.replace(directory, '').lstrip('/')
+        for pattern in patterns:
+            result.extend([os.path.join(child_root, name)
+                           for name in fnmatch.filter(files, pattern)])
+    return result
 
 setup(
     name='django-painless-seo',
-    version='0.0.3',
+    version='0.0.4',
     author='Glamping Hub',
     author_email='it@glampinghub.com',
-    packages=['painlessseo'],
+    packages=find_packages('.'),
+    include_package_data=True,
+    package_data={
+        '': recursive_include('painlessseo', [
+            '*.html', '*.css', '*.js', '*.txt', '*.png', '*.ico', '*.wsgi',
+            '*.xml', '*.gif', '*.jpg', '*.otf', '*.svg', '*.example', '*.woff',
+            '*.md',
+        ])
+    },
     url='https://github.com/Glamping-Hub/django-painless-seo',
     license='BSD',
     classifiers=[
