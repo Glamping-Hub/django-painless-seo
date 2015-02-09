49ea4ca9d22c934dcc87112c498e6ca5e8d74e1c
Alejandro Blanco <alejandro.b.e@gmail.com>
2015-02-09 10:45:58 +0100

    Prepare setup.py for pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 from setuptools import setup
 
 setup(
-    name='Django SimpleSEO',
+    name='django-simple-seo',
     version='0.0.1',
     author='Glamping Hub',
     author_email='it@glampinghub.com',
     packages=['simpleseo'],
     url='https://github.com/Glamping-Hub/django-simple-seo',
-    license='LICENSE',
-    description='Simple SEO app for django framework',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2 :: Only',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet'
+    ],
+    description='Simple SEO app for Django framework',
+    keywords='seo django',
     long_description=open('README.md').read(),
     requires=[
         'Django (>=1.5.0)',
