5669a74c7424c4a0591239084ed95069ca24c832
Antonio H M <antonio.hinojo.montero@gmail.com>
2016-11-24 12:21:11 +0100

    Added inline 'SeoMetadataWithValidationInline', which validates fields in SEO_ADMIN_FORM_REQUIRED_FIELDS setting as required.

--- painlessseo/admin.py
+++ painlessseo/admin.py
@@ -22,28 +22,32 @@ def get_language(obj):
     return mark_safe('<strong>%s</strong>' % language.upper())
 
 
-class SEOMetadataForm(forms.ModelForm):
-    def clean(self):
-        cleaned_data = super(SEOMetadataForm, self).clean()
-        obj = cleaned_data.get('id')
+class SEOMetadataWithValidationForm(forms.ModelForm):
+
+    def validate_required_fields(self, cleaned_data, fields):
         error_msg = _(u'This field is required')
+        for field in fields:
+            value = cleaned_data.get(field)
+            if not value:
+                if django.VERSION >= (1, 7):
+                    self.add_error(field, error_msg)
+                else:
+                    self._errors[field] = self.error_class([error_msg])
+                    del cleaned_data[field]
+        return cleaned_data
+
+    def clean(self):
+        cleaned_data = super(SEOMetadataWithValidationForm, self).clean()
+        obj = cleaned_data.get('id')
         for lang, fields in settings.SEO_ADMIN_FORM_REQUIRED_FIELDS.items():
             if obj.lang_code == lang:
-                for field in fields:
-                    value = cleaned_data.get(field)
-                    if not value:
-                        if django.VERSION >= (1, 7):
-                            self.add_error(field, error_msg)
-                        else:
-                            self._errors[field] = self.error_class([error_msg])
-                            del cleaned_data[field]
+                cleaned_data = self.validate_required_fields(cleaned_data, fields)
         return cleaned_data
 
 
 class SeoMetadataInline(GenericStackedInline):
     exclude = ('path', 'lang_code', )
     extra = 0
-    form = SEOMetadataForm
     max_num = 0
     model = SeoMetadata
 
@@ -56,6 +60,10 @@ class SeoMetadataInline(GenericStackedInline):
     def language(self, obj):
         return get_language(obj)
     language.short_description = 'Language'
+
+
+class SeoMetadataWithValidationInline(SeoMetadataInline):
+    form = SEOMetadataWithValidationForm
 
 
 class SeoMetadataAdmin(admin.ModelAdmin):
